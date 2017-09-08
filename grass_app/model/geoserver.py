02d1edbbf072e99b6a152c047dbc884c688621c5
Yanfu Zhou <yanfu.zhou@outlook.com>
2017-09-07 23:40:42 -0500

    Add docker.for.mac.localhost as localhost!

--- grass_app/model/geoserver.py
+++ grass_app/model/geoserver.py
@@ -4,5 +4,5 @@ if os.uname()[0] == 'Darwin':
     GEOSERVER_HOST = 'localhost'
     GEOSERVER_URL = 'http://localhost/geoserver/wcs'
 if os.uname()[0] == 'Linux':
-    GEOSERVER_HOST = 'docker'
+    GEOSERVER_HOST = 'docker.for.mac.localhost'
 GEOSERVER_URL = 'http://' + GEOSERVER_HOST + '/geoserver/wcs'
